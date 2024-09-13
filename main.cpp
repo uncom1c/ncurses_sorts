7def74b193b6275667167c92515a2c694cf64fd5
uncom1c <mishartem2004@gmail.com>
2024-09-13 18:45:15 +0300

    added hint

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@
 int main(int argc, char *argv[])
 {
     if (argc < 2){
-        std::cout<<"need more argumets lol";
+        std::cout<<"need more argumets lol"<<std::endl;
+        std::cout<<"A viable list of options you can use:"<<std::endl;
+        std::cout<<"./Kartinka <gnome, selection, insert, bubble, cocktail, quick, shell, stooge, slow, odd-even, bogo>"<<std::endl;
         return 0;
     }
     initscr();
@@ -29,49 +31,70 @@ int main(int argc, char *argv[])
     WINDOW* win = newwin(row,col,0,0);
     x = row;
     y=0;
+    bool seen_cmd = false;
     if (!strcmp(argv[1], "gnome")){
+        seen_cmd=true;
         gnome_sort(win, arr, size, x , y);
     } 
     if (!strcmp(argv[1], "selection")){
         selectionSort(win, arr, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "insert")){
         insertSort(win, arr, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "bubble")){
         bubbleSort(win, arr, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "cocktail")){
         cocktailSort(win, arr, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "quick")){
         int left = 0;
         int right = size-1;
         quickSort(win, arr, left, right, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "shell")){
         ShellSort(win, arr, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "stooge")){
         int left = 0;
         int right = size-1;
         stooge_sort(win, arr, left, right, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "slow")){
         int left = 0;
         int right = size-1;
         slow_sort(win, arr, left, right, size, x , y);
+        seen_cmd=true;
     } 
-    if (!strcmp(argv[1], "odd_even")){
+    if (!strcmp(argv[1], "odd-even")){
         odd_even_sort(win, arr, size, x , y);
+        seen_cmd=true;
     } 
     if (!strcmp(argv[1], "bogo")){
+        seen_cmd=true;
         bool is_sorted = false;
         while (!is_sorted){
             bogo_sort(win, arr, size, x , y, is_sorted);
             numbers(arr);
         }
-    } 
+    }
+    if (seen_cmd == false){
+
+        seen_cmd=true;
+        bool is_sorted = false;
+        while (!is_sorted){
+            bogo_sort(win, arr, size, x , y, is_sorted);
+            numbers(arr);
+        }
+    }
     getch(); // ждём нажатия символа
     endwin();
 
